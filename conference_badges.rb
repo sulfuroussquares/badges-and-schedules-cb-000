7cd7e01b54d157e66f897f4206165c517d0ac9eb
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-01 19:43:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,7 +27,6 @@ def printer(names)
   puts assign_rooms(names)
 end
 
-printer(names)
 #Now you have to tell the printer what to print.
 #Create a method called printer that will output first the results of the batch_badge_creator method
 #and then of the assign_rooms method to the screen.
