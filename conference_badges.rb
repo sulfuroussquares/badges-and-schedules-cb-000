f7d80a50254bf00e6487c62a9c15507e48899f68
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-01 19:23:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  newArray = []
+  names.each do |name|
+    newArray << "Hello, my name is #{name}."
+  end
+  return newArray
+end
+
+
+
+def assign_rooms(names)
+  newArray = []
+  i = 0
+names.each do |name|
+  newArray << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  i+= 1
+end
+return newArray
+end
