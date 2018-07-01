3969c90e7743eca1a48347bef1796c758823b510
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-01 19:27:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,16 +24,16 @@ end
 
 def printer(names, rooms)
 
-    
+
   end
-  
+
 end
 
 
-#Now you have to tell the printer what to print. 
-#Create a method called printer that will output first the results of the batch_badge_creator method 
+#Now you have to tell the printer what to print.
+#Create a method called printer that will output first the results of the batch_badge_creator method
 #and then of the assign_rooms method to the screen.
-#Hint: Remember that methods can call other methods. 
-#If the return value of assign_rooms is an array of room assignments, 
-#how can you print out each assignment? 
+#Hint: Remember that methods can call other methods.
+#If the return value of assign_rooms is an array of room assignments,
+#how can you print out each assignment?
 #You'll need to iterate over your array of room assignments in order to puts out each individual assignment.
